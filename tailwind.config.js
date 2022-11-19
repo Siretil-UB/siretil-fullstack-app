234ce9d00c449c2d310584cd6c71ebb38d1d7e39
Nafi Baliya <baliya363@gmail.com>
2022-11-19 18:02:02 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
+    purge: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    darkMode: false, // or 'media' or 'class'
     theme: {
+        extend: {},
+    },
+    variants: {
         extend: {},
     },
     plugins: [],
