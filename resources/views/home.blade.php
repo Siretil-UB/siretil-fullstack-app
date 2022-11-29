23e943e4ee24e6304031411c5166eb2a8c1969d5
Nafi Baliya <baliya363@gmail.com>
2022-11-29 12:45:04 +0700

    update homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    <p class="mt-5 font-roboto-slab text-4xl font-bold">SELAMAT DATANG, NAMA</p>
+    <div class="mt-8 flex flex-row flex-wrap justify-between gap-y-5">
+        <a href="" class="h-[270px] w-[48%] rounded bg-gray-200 shadow-md">
+        </a>
+        <a href="" class="h-[270px] w-[48%] rounded bg-gray-200 shadow-md">
+        </a>
+        <a href="" class="h-[270px] w-[48%] rounded bg-gray-200 shadow-md">
+        </a>
+        <a href="" class="h-[270px] w-[48%] rounded bg-gray-200 shadow-md">
+        </a>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
 
 Route::post("/login", [LoginController::class, 'authenticate'])->name('login');
 Route::get("/login", [LoginController::class, 'getForm']);
