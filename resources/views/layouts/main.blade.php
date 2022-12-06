22c7846cf624d1274ddf1b5dc6d5d7ec2affe579
Nafi Baliya <baliya363@gmail.com>
2022-12-06 08:50:11 +0700

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,10 +13,13 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 </head>
 
 <body class="">
-    <nav class="absolute top-0 left-0 bottom-0 w-[80px] bg-blue-800 py-10"></nav>
+    <nav class="absolute top-0 left-0 bottom-0 w-[80px] bg-blue-800 py-10">
+        
+    </nav>
     <div class="mx-auto h-screen w-[1100px] py-10">
         <h1 class="text-2xl font-extrabold italic text-orange-400">SISTEM REKRUTMEN TIM LOMBA</h1>
         <h2 class="text-2xl font-bold italic text-blue-800">UNIVERSITAS BRAWIJAYA</h2>
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
 
 Route::post("/login", [LoginController::class, 'authenticate'])->name('login');
 Route::get("/login", [LoginController::class, 'getForm']);
+Route::delete('logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
 
 // protected route
 Route::middleware('auth')->group( function () {
