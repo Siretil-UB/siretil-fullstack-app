4bb17e6ace20ca3c96fe7c7bdd164c26921184c6
mafibal <baliya363@gmail.com>
2022-12-09 16:18:45 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,11 +14,29 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="">
-    <nav class="absolute top-0 left-0 bottom-0 w-[80px] bg-blue-800 py-10">
-        
+    <nav class="absolute top-0 left-0 bottom-0 flex h-full w-[80px] flex-col gap-8 bg-blue-800 py-10">
+        <a href="" class="flex w-full items-center justify-center text-4xl text-white">
+            <i class="fa-solid fa-house"></i>
+        </a>
+        <a href="" class="flex w-full items-center justify-center text-4xl text-white">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </a>
+        <a href="" class="flex w-full items-center justify-center text-4xl text-white">
+            <i class="fa-solid fa-users"></i>
+        </a>
+        <a href="" class="flex w-full items-center justify-center text-4xl text-white">
+            <i class="fa-sharp fa-solid fa-user"></i>
+        </a>
+        <a href="" class="flex w-full items-center justify-center text-4xl text-white">
+            <i class="fa-solid fa-bell"></i>
+        </a>
     </nav>
     <div class="mx-auto h-screen w-[1100px] py-10">
         <h1 class="text-2xl font-extrabold italic text-orange-400">SISTEM REKRUTMEN TIM LOMBA</h1>
